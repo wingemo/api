d298123e9c65bc33201c4b8119393b2fec39cc02
Wingemo <philip.wingemo@outlook.com>
2022-10-11 22:56:49 +0200

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,5 +1,3 @@
-package com.example.accessingdatamysql;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
