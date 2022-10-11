0943771c57a2993ae6877275e00370403bcbd996
Wingemo <philip.wingemo@outlook.com>
2022-10-11 22:56:34 +0200

    Update UserController.java

--- UserController.java
+++ UserController.java
@@ -1,11 +1,32 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-public class HelloWorldController {
+@Controller 
+@RequestMapping(path="/demo") 
+public class MainController {
+  @Autowired 
+  private UserRepository userRepository;
 
-   @RequestMapping("/")
-   public String helloWorld(){
-       return "Hello World!!";
-   }
+  @PostMapping(path="/add")
+  public @ResponseBody String addNewUser (@RequestParam String name
+      , @RequestParam String email) {
+
+    User n = new User();
+    n.setName(name);
+    n.setEmail(email);
+    userRepository.save(n);
+    return "Saved";
+  }
+
+  @GetMapping(path="/all")
+  public @ResponseBody Iterable<User> getAllUsers() {
+    return userRepository.findAll();
+  }
 }
