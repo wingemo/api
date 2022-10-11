bf83af5c68326de3728511cd17a2ad36f463262c
Wingemo <philip.wingemo@outlook.com>
2022-10-11 21:14:39 +0200

    Update HelloWorldController.java

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -1,7 +1,6 @@
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HelloWorldController {
 
