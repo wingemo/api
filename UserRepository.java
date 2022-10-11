f1586e5e25028bba90f8be8cb79b65a4b402434e
Wingemo <philip.wingemo@outlook.com>
2022-10-11 21:19:30 +0200

    Create UserRepository.java

---
+++ UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.accessingdatamysql.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
