58cb8cf1ca5d2e0492d40120a568af98212bca2f
Wingemo <philip.wingemo@outlook.com>
2022-10-11 21:17:07 +0200

    Update UserModel.java

--- UserModel.java
+++ UserModel.java
@@ -3,7 +3,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity 
 public class User {
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
